69e6e62335d561f79a97c03360f7052a841f56f9
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2018-07-29 13:10:56 +0300

    Adding tests for is_valid_employee_provided()
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- src/helpers.rs
+++ src/helpers.rs
@@ -11,3 +11,17 @@ pub fn get_user_input() -> String {
 pub fn is_valid_employee_provided(vector: &Vec<&str>) -> bool {
     return vector.len() == 4 && vector[0] == "Add" && vector[2] == "to";
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_valid_employee_provided() {
+        assert_eq!(is_valid_employee_provided(&vec!["Add", "George", "to", "Sales"]), true);
+        assert_eq!(is_valid_employee_provided(&vec!["Add", "George", "to"]), false);
+        assert_eq!(is_valid_employee_provided(&vec!["add", "George", "to", "Sales"]), false);
+        assert_eq!(is_valid_employee_provided(&vec!["Add", "George", "To", "Sales"]), false);
+        assert_eq!(is_valid_employee_provided(&vec!["Add", "George", "To", "Sales"]), false);
+    }
+}
