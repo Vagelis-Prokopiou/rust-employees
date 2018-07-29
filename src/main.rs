650a7cf4ac6ab196ff343db42914956e8cb39f2c
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2018-07-29 10:35:32 +0300

    Setting the basic branches of execution
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,11 @@ use std::io;
 
 fn main() {
     let mut employees_database: HashMap<String, Vec<String>> = HashMap::new();
-    let mut user_input: String = String::new();
+//    let mut user_selection: String = String::new();
+    let mut user_provided_employee: String = String::new();
 
     loop {
+        let mut user_selection: String = String::new();
         println!("What would you like to do?");
         println!("Press 1 to add a new employee.");
         println!("Press 2 to retrieve all existing employees per department.");
@@ -23,23 +25,37 @@ fn main() {
         println!("Please provide your selection:");
 
         io::stdin()
-            .read_line(&mut user_input)
+            .read_line(&mut user_selection)
             .expect("Failed to read line.");
+        let user_selection = user_selection.trim();
 
-        let tokens: Vec<&str> = user_input.trim().split(' ').collect();
-        let employee_name: String = tokens[1].to_owned();
-        let employee_department: String = tokens[3].to_owned();
+        if user_selection == "1" {
+            let tokens: Vec<&str> = user_provided_employee.trim().split(' ').collect();
 
-        if employees_database.contains_key(&employee_department) {
-            println!("employees_database.contains_key");
-            let employees: &mut Vec<String> = employees_database.get_mut(&employee_department).unwrap();
-            employees.push(employee_name);
+            println!("{:?}", tokens);
+
+            let employee_name: String = tokens[1].to_owned();
+            let employee_department: String = tokens[3].to_owned();
+
+            if employees_database.contains_key(&employee_department) {
+                println!("employees_database.contains_key");
+                let employees: &mut Vec<String> = employees_database.get_mut(&employee_department).unwrap();
+                employees.push(employee_name);
+            } else {
+                println!("NOT employees_database.contains_key");
+                employees_database.insert(employee_department, vec![employee_name]);
+            }
+
+            println!("{:?}", employees_database);
+            break;
+        } else if user_selection == "2" {
+            println!("2 was selected");
+        } else if user_selection == "3" {
+            println!("3 was selected");
+        } else if user_selection == "q" {
+            break;
         } else {
-            println!("NOT employees_database.contains_key");
-            employees_database.insert(employee_department, vec![employee_name]);
+            println!("Wrong selection. Please, try again.");
         }
-
-        println!("{:?}", employees_database);
-        break;
     }
 }
